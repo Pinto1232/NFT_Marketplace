62a27572823eaa4d570571ae63fbe394f1138d09
Pinto Manuel <pintoydc@gmail.com>
2025-01-26 22:58:48 +0200

    fixed

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -7,7 +7,7 @@ const Dashboard: React.FC = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
   const theme = useTheme();
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm')); 
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
   const toggleSidebar = () => {
     setIsSidebarOpen((prev) => !prev);
@@ -15,15 +15,16 @@ const Dashboard: React.FC = () => {
 
   return (
     <Box sx={{ display: 'flex', height: '100vh', backgroundColor: '#14181C' }}>
-      {/* Sidebar */}
       <Sidebar
         isOpen={isSidebarOpen}
         onClose={toggleSidebar}
-        variant={isSmallScreen ? 'temporary' : 'permanent'} 
+        variant={isSmallScreen ? 'temporary' : 'permanent'}
       />
-      {/* Main Content */}
       <Box component="main" sx={{ flexGrow: 1 }}>
-        <DashboardContent toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
+        <DashboardContent 
+           toggleSidebar={toggleSidebar} 
+           isSidebarOpen={isSidebarOpen} 
+        />
       </Box>
     </Box>
   );
